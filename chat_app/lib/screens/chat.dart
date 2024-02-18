dccb2cfe6758a266d70ac35d60904a8dda5cab7e
Lau2906 <79776871+Lau2906@users.noreply.github.com>
2024-02-18 15:58:32 -0500

    Notificaciones

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,10 +1,32 @@
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setUpPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +43,13 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text('Logged in!'),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage()
+        ],
       ),
     );
   }
